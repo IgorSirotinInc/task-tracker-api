d4a9bfc7af31e1ef9663754c34678edda96a0410
IgorSirotin <theneetfreekgames@gmail.com>
2026-03-29 20:09:01 +0300

    BACK-004 Добавил таблицу Task. Настроил конфигурацию. Добавил миграцию.

--- src/TaskTracker.Migrations/Migrations/20260329170448_add-task-table.Designer.cs
+++ src/TaskTracker.Migrations/Migrations/20260329170448_add-task-table.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskTracker.Storage.Context;
@@ -11,9 +12,11 @@ using TaskTracker.Storage.Context;
 namespace TaskTracker.Migrations.Migrations
 {
     [DbContext(typeof(TaskTrackerContext))]
-    partial class TaskTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20260329170448_add-task-table")]
+    partial class addtasktable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
